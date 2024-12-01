a21e7491ca57417c30f1af41ba48e36c0c09d734
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2024-12-01 22:48:56 +0100

    net8 bruh

--- ShockAlarm/frontend/build_frontend.sh
+++ ShockAlarm/frontend/build_frontend.sh
@@ -1,8 +1,8 @@
 npm i
 npm run build
-rm -rf ../bin/Release/net6.0/frontend
-rm -rf ../bin/Debug/net6.0/frontend
-mkdir -p ../bin/Release/net6.0/frontend
-mkdir -p ../bin/Debug/net6.0/frontend
-cp -r build/* ../bin/Release/net6.0/frontend/
-cp -r build/* ../bin/Debug/net6.0/frontend/
+rm -rf ../bin/Release/net8.0/frontend
+rm -rf ../bin/Debug/net8.0/frontend
+mkdir -p ../bin/Release/net8.0/frontend
+mkdir -p ../bin/Debug/net8.0/frontend
+cp -r build/* ../bin/Release/net8.0/frontend/
+cp -r build/* ../bin/Debug/net8.0/frontend/
