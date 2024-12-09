0172f3492eaa37048e28760b354304965b106414
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2024-12-09 17:24:26 +0100

    Add new migration

---
+++ ShockAlarm/Migrations/20241209162404_idk.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShockAlarm.Migrations
+{
+    /// <inheritdoc />
+    public partial class idk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Shockers_ShockerId",
+                table: "Shockers",
+                column: "ShockerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Shockers_ShockerId",
+                table: "Shockers");
+        }
+    }
+}
